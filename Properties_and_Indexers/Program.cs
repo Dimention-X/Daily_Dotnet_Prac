ebe8d6aa75ba62f926f9d86847c56a6edb8f3c6f
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2017-06-21 10:26:16 +0000

    Normal Operations of the prop .

---
+++ Properties_and_Indexers/Program.cs
@@ -0,0 +1,37 @@
+﻿// properties are the extension to the fields, uses accessors for the set and get the value for the field. 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_and_Indexers
+{
+    public class Student
+    {
+        private string code = "N.A";
+        private string name = "N.A";
+        private int age = 0;
+
+        public int Age { get { return age; } set { age = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public string Code { get { return code; } set { code = value; } }
+
+        public override string ToString()
+        {
+            return "Code = " + Code +"\n" + "Name =" + Name + "\n" + "Age =" + Age;
+        }
+    }
+
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.Code = "C#";
+            s.Name = "Akhilesh KS";
+            s.Age = 24;
+            Console.WriteLine(s);     
+        }
+    }
+}
